9c13708d5640bd4407970a1c51b0ba51e30367b3
DiositoDiego <20213tn096@utez.edu.mx>
2022-11-30 22:02:46 -0600

    Implementacion de orderRouter a routes

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -1,1 +1,6 @@
-// TODO: reqerir los controladores de cada ruta y asignarlos a las rutas (exportar el router)
+// TODO: requerir los controladores de cada ruta y asignarlos a las rutas (exportar el router)
+const { orderRouter } = require('./controller/orders/orders.controller.js');
+
+module.exports = {
+    orderRouter
+}
