d3e77f59824a58ff3986b5468c826619619765cc
FReptar0 <20213tn137@utez.edu.mx>
2022-12-01 21:53:01 -0600

    Correccion de rutas -FReptar0

--- src/modules/controller/routes.js
+++ src/modules/controller/routes.js
@@ -1,5 +1,5 @@
 // TODO: requerir los controladores de cada ruta y asignarlos a las rutas (exportar el router)
-const { orderRouter } = require('./controller/orders/orders.controller.js');
+const { orderRouter } = require('./orders/orders.controller.js');
 
 module.exports = {
     orderRouter
