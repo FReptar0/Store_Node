47aa91f42b4f0cb80264d300c31a082294dbf3f0
yahird13 <21213tn102@utez.edu.mx>
2022-12-13 12:49:23 -0600

    Cambios del Puchis

--- src/modules/controller/rol/rol.gateway.js
+++ src/modules/controller/rol/rol.gateway.js
@@ -1,1 +1,58 @@
+const { query } = require('express')
 const { sql } = require('../../../utils/MySQL');
+
+const findAll = async () => {
+  const query = `SELECT * from ROl`;
+
+  return await sql(query, []);
+};
+
+const findById = async (id) => {
+  if (!id) throw Error("Missing fields");
+  if (Number.isNaN(id)) throw Error("Wrong Type");
+  const query = `SELECT * FROM ROL Where ID_ROL=?`;
+  return await sql(query, [id]);
+};
+
+const save = async (rol) => {
+  if (
+    !rol.rolname 
+
+  )
+    throw Error("Missing field");
+  const query = `INSERT INTO ROL (ROLNAME) VALUES (?)`;
+  const { insertedId } = await sql(query, [
+    rol.rolname
+  ]);
+  return { ...rol}
+};
+
+const update = async (rol, id) => {
+  if (!id) throw Error('Missing fields');
+  if (Number.isNaN(id)) throw Error("Wrong Type")
+  if (
+    !rol.rolname
+) throw Error('Missing fields');
+  const query = `UPDATE ROL SET ROLNAME=? WHERE ID_ROL=?`;
+  await sql(query,[
+    rol.rolname,
+    id
+  ]);
+  return {...rol,id:id}
+}
+
+const remove = async(id)=>{
+  if(!id) throw Error('Missing fields');
+  if(Number.isNaN(id)) throw Error("Wrong Type");
+  const query = 'DELETE FROM ROL WHERE ID_ROL=?';
+  await sql(query,[id]);
+  return {idDeleted:id} 
+}
+
+module.exports = {
+  findAll,
+  findById,
+  save,
+  update,
+  remove
+};
