88a18b00144fab90acd7e52623a5500233332f12
joviicam <20213tn110@utez.edu.mx>
2022-12-13 09:48:30 -0600

    Cambios

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -1,1 +1,5 @@
 // TODO: reqerir los controladores de cada ruta y asignarlos a las rutas (exportar el router)
+const { providersRouter } = require('./providers/providers.controller')
+module.exports ={
+    providersRouter
+}
