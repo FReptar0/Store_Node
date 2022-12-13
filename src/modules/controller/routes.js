1453095c8ad5bfb9028f9bcb19559a5a8b3f018c
DiositoDiego <20213tn096@utez.edu.mx>
2022-12-13 09:58:34 -0600

    Borramiento del 'js' en mi router de routes

--- src/modules/controller/routes.js
+++ src/modules/controller/routes.js
@@ -1,5 +1,5 @@
 // TODO: requerir los controladores de cada ruta y asignarlos a las rutas (exportar el router)
-const { orderRouter } = require('./orders/orders.controller.js');
+const { orderRouter } = require('./orders/orders.controller');
 
 module.exports = {
     orderRouter
