24d9f76f1763cd7c075b239ebeda5b43e55adc5f
joviicam <20213tn110@utez.edu.mx>
2022-12-13 10:05:57 -0600

    rutas corregidas

--- src/modules/controller/routes.js
+++ src/modules/controller/routes.js
@@ -1,10 +1,14 @@
 // TODO: requerir los controladores de cada ruta y asignarlos a las rutas (exportar el router)
-const { orderRouter } = require('./orders/orders.controller.js');
-const { storeRouter } = require('./stores/stores.controller.js');
-const { productRouter } = require('./products/products.controller.js');
+const { orderRouter } = require('./orders/orders.controller');
+const { storeRouter } = require('./stores/stores.controller');
+const { productRouter } = require('./products/products.controller');
+const { providersRouter } = require('./providers/providers.controller')
+const {} = require('./rol/rol.controller')
 
 module.exports = {
     orderRouter,
     storeRouter,
     productRouter,
+    providersRouter,
+    
 }
